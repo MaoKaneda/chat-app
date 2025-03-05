100b3777910e97b418a69e731900b93a274e5079
Mao Kaneda <maddieqqie@gmail.com>
2025-03-05 13:59:09 +0900

    自分なりの解釈のまとめ

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
+
+＃Maddieの解説
+#routes.rbについては、devise_for :usersが追加されている。
+#自分で「  devise_for :users」記述することはない→deviseをインストールすると自動で追加されるため
+#
