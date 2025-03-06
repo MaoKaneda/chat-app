5ebf73a07382818d102f5b4e5c4fab43100f8ed9
Mao Kaneda <maddieqqie@gmail.com>
2025-03-06 22:00:57 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,18 +1,12 @@
-# チャットルームを管理するコントローラーです
-# チャットルームの一覧表示、新規作成などの機能を提供します
 class RoomsController < ApplicationController
 
-  # チャットルームの一覧を表示するページを表示します
   def index
   end
   
-  # 新しいチャットルームを作成するためのフォームを表示します
   def new
     @room = Room.new
   end
 
-  # 新しいチャットルームを作成します
-  # 作成が成功したら一覧ページに戻り、失敗したら作成フォームを再表示します
   def create
     @room = Room.new(room_params)
     if @room.save
@@ -22,10 +16,14 @@ class RoomsController < ApplicationController
     end
   end
 
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
   private
 
-  # チャットルーム作成時に許可するパラメータを設定します
-  # ルーム名と参加するユーザーのIDを保存できます
   def room_params
     params.require(:room).permit(:name, user_ids: [])
   end
