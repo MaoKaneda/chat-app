986056be1e5aeb3613ff56c1d2ac3e011d8d2251
Mao Kaneda <maddieqqie@gmail.com>
2025-03-17 01:38:33 +0900

    コメントアウト

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -1,18 +1,29 @@
+# このモジュールは、テストでユーザーログインを簡単に行うためのものです
 module SignInSupport
+  # sign_inメソッドは、テストの中でユーザーログインを行うための便利な機能です
+  # 引数の'user'には、ログインしたいユーザーの情報が入っています
   def sign_in(user)
-    # サインインページへ移動する
+    # まず、ログインページにアクセスします
+    # new_user_session_pathは、ログインページのURLを指します
     visit new_user_session_path
 
-    # すでに保存されているユーザーのemailとpasswordを入力する
+    # ログインフォームに、ユーザーのメールアドレスとパスワードを入力します
+    # fill_inは、フォームに文字を入力するための命令です
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
 
-    # ログインボタンをクリックする
+    # 「Log in」ボタンをクリックしてログインします
+    # click_onは、ボタンをクリックする命令です
     click_on('Log in')
+
+    # ログイン処理が完了するまで1秒待ちます
+    # これは、ページの読み込みを待つためです
     sleep 1
   end
 end
 
+# このモジュールを全てのテストで使えるようにする設定です
 RSpec.configure do |config|
+  # SignInSupportモジュールを、全てのテストで使用できるようにします
   config.include SignInSupport
 end
